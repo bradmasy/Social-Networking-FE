40e3b4f9990acbf2a79422c8d0eca7b388350385
Brad <bradleymasciotra@gmail.com>
2024-05-05 12:42:10 -0700

    trying change to target in compiler options to run tests

---
+++ src/pages/industry-info/IndustryInfo.tsx
@@ -0,0 +1,18 @@
+
+
+
+import { Header } from "../../components/index";
+
+import "./industry-info.scss";
+
+
+export const IndustryInfo:React.FC = () => {
+    return (
+        <>
+        <Header/>
+        <section className="ss-industry-info">
+
+        </section>
+        </>
+    )
+}
