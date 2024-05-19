f6945798fe4feade06a641aae22b70410599c687
Brad <bradleymasciotra@gmail.com>
2024-05-19 13:59:48 -0700

    adding consoles

--- src/components/form/payment-form/SSPaymentForm.tsx
+++ src/components/form/payment-form/SSPaymentForm.tsx
@@ -62,12 +62,13 @@ export const SSPaymentForm: React.FC<SSPaymentFormProps> = (props) => {
                             // type:props.type
                         }
 
-
+                        console.log(body)
                         // disable the button while the transaction is occuring...
                         setEnableButton(true);
 
                         apiService.make_payment(body)
                             .then((response) => {
+                                console.log(response)
                                 setDisplay(true);
                             })
                             .catch((error) => {
